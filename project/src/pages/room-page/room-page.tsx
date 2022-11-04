1b81bec0e2e5d865646dcb8a1a9bac6422527853
Alexander Kungurov <104078438+AxlMonk@users.noreply.github.com>
2022-11-04 21:30:30 +0500

    ÐПравÐски

--- project/src/pages/room-page/room-page.tsx
+++ project/src/pages/room-page/room-page.tsx
@@ -226,8 +226,7 @@ function RoomPage(): JSX.Element {
                           </svg>
                         </label>
                       </div>
-                      <textarea className="reviews__textarea form__textarea" id="review" name="review"
-                                placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+                      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
                       <div className="reviews__button-wrapper">
                         <p className="reviews__help">
                           To submit review please make sure to set <span className="reviews__star">rating</span> and
@@ -257,8 +256,7 @@ function RoomPage(): JSX.Element {
                           <b className="place-card__price-value">&euro;80</b>
                           <span className="place-card__price-text">&#47;&nbsp;night</span>
                         </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                                type="button">
+                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                           <svg className="place-card__bookmark-icon" width="18" height="19">
                             <use xlinkHref="#icon-bookmark"></use>
                           </svg>
@@ -281,8 +279,7 @@ function RoomPage(): JSX.Element {
                   <article className="near-places__card place-card">
                     <div className="near-places__image-wrapper place-card__image-wrapper">
                       <a href="#">
-                        <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200"
-                             alt="Place image" />
+                        <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image" />
                       </a>
                     </div>
                     <div className="place-card__info">
@@ -317,8 +314,7 @@ function RoomPage(): JSX.Element {
                     </div>
                     <div className="near-places__image-wrapper place-card__image-wrapper">
                       <a href="#">
-                        <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200"
-                             alt="Place image" />
+                        <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image" />
                       </a>
                     </div>
                     <div className="place-card__info">
